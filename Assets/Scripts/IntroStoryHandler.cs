57e10ca09b283b9011289a5bd60947bc5997ee7b
Zean Isnomo <zeanfender11@gmail.com>
2026-05-07 11:22:21 +0700

    a LOT of them that its hard to describe

--- Assets/Scripts/IntroStoryHandler.cs
+++ Assets/Scripts/IntroStoryHandler.cs
@@ -4,6 +4,23 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 
+public enum CreditureType
+{
+    Image,
+    Text,
+}
+
+[System.Serializable]
+public class Crediture
+{
+    public float startTime;
+    public float duration;
+    public CreditureType creditureType;
+    public string role;
+    public string content;
+    public Sprite imageContentIfAvailable;
+}
+
 [System.Serializable]
 public class IStoryCueHandler
 {
@@ -20,6 +37,8 @@ public class IntroStoryHandler : MonoBehaviour
     public List<IStoryCueHandler> storyCues = new List<IStoryCueHandler>();
     public float alphaDuration = 0.25f;
     public GameObject mainGameEntry;
+    [SerializeField]
+    public List<Crediture> credits = new List<Crediture>();
     private TMP_Text storyText;
     private AudioSource narrationSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -66,6 +85,64 @@ public class IntroStoryHandler : MonoBehaviour
                 RawImage rawImage = GameObject.Find("RawImage").GetComponent<RawImage>();
                 rawImage.CrossFadeAlpha(0f, 2f, false);
                 StartCoroutine(DeactivateAfterDelay(2f));
+            }
+        }
+        // Run the credits the same thread along with the story cues
+        for (int i = 0; i < credits.Count; i++)
+        {
+            float elapsed = narrationSource.time;
+            float start = credits[i].startTime;
+            float end = credits[i].startTime + credits[i].duration;
+
+            // Find the hierarchy objects
+            GameObject creditureThing = GameObject.Find("CreditureThing");
+            if (creditureThing == null) continue;
+
+            TMP_Text roleText = creditureThing.transform.Find("TextSub")?.GetComponent<TMP_Text>();
+            Transform subSel = creditureThing.transform.Find("Crediture_SubSel");
+            Image contentImage = subSel?.Find("Image")?.GetComponent<Image>();
+            TMP_Text contentText = subSel?.Find("Text")?.GetComponent<TMP_Text>();
+
+            if (elapsed >= start && elapsed <= end)
+            {
+                // Populate content on first frame of entry
+                if (roleText != null) roleText.text = credits[i].role;
+
+                if (credits[i].creditureType == CreditureType.Image)
+                {
+                    if (contentImage != null)
+                    {
+                        contentImage.gameObject.SetActive(true);
+                        if (credits[i].imageContentIfAvailable != null)
+                            contentImage.sprite = credits[i].imageContentIfAvailable;
+                    }
+                    if (contentText != null) contentText.gameObject.SetActive(false);
+                }
+                else
+                {
+                    if (contentText != null)
+                    {
+                        contentText.gameObject.SetActive(true);
+                        contentText.text = credits[i].content;
+                    }
+                    if (contentImage != null) contentImage.gameObject.SetActive(false);
+                }
+
+                // Fade in at start
+                if (elapsed < start + alphaDuration)
+                {
+                    roleText?.CrossFadeAlpha(1f, alphaDuration, false);
+                    contentImage?.CrossFadeAlpha(1f, alphaDuration, false);
+                    contentText?.CrossFadeAlpha(1f, alphaDuration, false);
+                }
+
+                // Fade out near end
+                if (elapsed >= end - alphaDuration)
+                {
+                    roleText?.CrossFadeAlpha(0f, alphaDuration, false);
+                    contentImage?.CrossFadeAlpha(0f, alphaDuration, false);
+                    contentText?.CrossFadeAlpha(0f, alphaDuration, false);
+                }
             }
         }
     }
