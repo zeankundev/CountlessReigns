55c7c87868db91bfdaa95a8a89d285926277d348
Zean Isnomo <zeanfender11@gmail.com>
2026-05-08 08:33:04 +0700

    hope this will resolve the fading issue

--- Assets/Scripts/IntroStoryHandler.cs
+++ Assets/Scripts/IntroStoryHandler.cs
@@ -61,6 +61,7 @@ public class IntroStoryHandler : MonoBehaviour
     IEnumerator DeactivateAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
+        GameObject.Find("RawImage").SetActive(false);
     }
 
     // Update is called once per frame
